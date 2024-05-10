978651bed94c4b7261a5d7335a9ce123f7a7824b
Renty <Renty@BJ-44B5QD3-708>
2024-05-10 16:42:14 +0800

    添加项目文件。

---
+++ Designer/Program.cs
@@ -0,0 +1,13 @@
+﻿// Ignore Spelling: RTYC
+
+using System;
+using System.Windows.Forms;
+
+namespace RTYC.Designer
+{
+	internal static class Program
+	{
+		internal static Form FormMain;
+		[STAThread] internal static void Main () { Application.EnableVisualStyles(); Application.SetCompatibleTextRenderingDefault(false); FormMain = new WinMain(); Application.Run(FormMain); }
+	}
+}
---
+++ Designer/WinMain.Designer.cs
@@ -0,0 +1,29 @@
+﻿// Ignore Spelling: RTYC
+using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace RTYC.Designer
+{
+	partial class WinMain
+	{
+		private IContainer C = null;
+		/// <summary>清理所有正在使用的资源。</summary>
+		/// <param name="D">如果应释放托管资源，为 true；否则为 false。</param>
+		protected override void Dispose (Boolean D) { if (D && (C != null)) C.Dispose(); base.Dispose(D); }
+
+		#region Windows 窗体设计器生成的代码
+
+		private void InitializeComponent ()
+		{
+			this.C = new Container();
+			this.AutoScaleMode = AutoScaleMode.Font;
+			this.ClientSize = new Size(800, 450);
+			this.Text = "Form1";
+		}
+
+		#endregion
+	}
+}
+
